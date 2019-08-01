be84c3c50de8a0627f207558a7c728017cf7f085
Marco Chavez <unsettlesea@gmail.com>
2019-07-31 20:14:34 -0700

    setup files to add backend

--- client/dist/final-project.e31bb0bc.js
+++ client/dist/final-project.e31bb0bc.js
@@ -29938,7 +29938,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50222" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60110" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
