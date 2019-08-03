d2cd1f748cdd81348eceb5312cb641dd97ee89d0
Marco Chavez <unsettlesea@gmail.com>
2019-08-03 10:17:49 -0700

    added some story

--- client/screens/AboutMe.js
+++ client/screens/AboutMe.js
@@ -26,7 +26,13 @@ function AboutMe(props) {
               me how to run java applications through the CLI because my favorite game was minecraft which ran on Java. During this period in time Minecraft was
               never intended to run on a linux system, because of this if I wanted to play Minecraft I had to manually start a java virtual machine. 
               Ever since then I had to learn how to work around all of the limitations of working with a linux machine which meant I had to learn all about
-              the linux CLI.
+              the linux CLI. Ever since then I took it upon myself to also build plugins for minecraft servers using Java, and using some basic HTML, CSS, and JavaScript
+              to create websites for those servers. As I started getting more and more interested in programming I realized that I could do something amazing
+              with all of this knowledge, and so I made the decision to start going to school for programming.
+            </p>
+            <p>I am now about to graduate from this amazing program called Woz U, and what I want to do is build something for other people to follow in my footsteps.
+              Tech isn't important where I am from, and there is no impact that software developers really make here, so getting into programming was very difficult for
+              me because no one knew why it mattered to me.
             </p>
           </section>
         </div>
